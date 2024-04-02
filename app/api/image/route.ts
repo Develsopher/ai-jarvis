3e1669ffbcf5c10194182679b65385e81e5f36cf
junhyeongchae <jchae7184@gmail.com>
2024-04-02 23:55:59 +0900

    image generator 추가

---
+++ app/api/image/route.ts
@@ -0,0 +1,44 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = 1, resolution = "512x512" } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!openai.apiKey) {
+      return new NextResponse("OpenAI API Key is missing", { status: 500 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Prompt are required", { status: 400 });
+    }
+    if (!amount) {
+      return new NextResponse("Amount are required", { status: 400 });
+    }
+    if (!resolution) {
+      return new NextResponse("Resolution are required", { status: 400 });
+    }
+
+    const response = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: resolution,
+    });
+    console.log("hello", response);
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.log("[CONVERSATION_ERROR]", error);
+    return new NextResponse("Inter Server Error", { status: 500 });
+  }
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: ["oaidalleapiprodscus.blob.core.windows.net"],
+  },
+};
 
 export default nextConfig;
+
+// oaidalleapiprodscus.blob.core.windows.net
