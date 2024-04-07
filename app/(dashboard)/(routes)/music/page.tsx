ea02926b95033644963e196290e71ba40c8e418d
junhyeongchae <jchae7184@gmail.com>
2024-04-07 22:20:34 +0900

    music generator 추가

---
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import * as z from "zod";
+import axios from "axios";
+import { MessageCircleMoreIcon, Music2 } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import { Heading } from "@/components/heading";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/Loader";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+
+const MusicPage = () => {
+  const router = useRouter();
+  const [music, setMusic] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setMusic(undefined);
+
+      const response = await axios.post("/api/music", values);
+
+      setMusic(response.data.audio);
+
+      form.reset();
+    } catch (error: any) {
+      // TODO 결제유도 모달 띄우기
+      console.error(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Music"
+        description="Jarvis, let's play some music!"
+        icon={Music2}
+        iconColor="text-sky-500"
+        bgColor="bg-sky-500/10"
+      />
+      <div className="px-3 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="강아지 소리를 내봐!"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-primary">
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && (
+            <Empty
+              label="No music started yet!
+          "
+            />
+          )}
+          {music && (
+            <audio controls className="w-full mt-8">
+              <source src={music} />
+            </audio>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MusicPage;
